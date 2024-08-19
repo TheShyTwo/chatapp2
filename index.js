93b916b313ed710a942ea86f38b470b93fbbc509
TheShyy2 <longbtceth100199@gmail.com>
2024-08-20 00:30:41 +0700

    update

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const http = require('http')
+const path = require('path')
+const {Server} = require('socket.io')
 
 const port = 3000
 const host = '127.0.0.1'
 
 const app = express()
 const server = http.createServer(app)
+const io = new Server(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.send('hello world vip')
+    res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+    console.log('user connected !')
+    socket.on('on-chat', data => {
+        io.emit('user-chat', data)
+    })
 })
 
 server.listen(port, () => {
